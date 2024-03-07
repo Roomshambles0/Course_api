c7539997e4516b269e2509304b81033652e9c340
pk <piyushkulkarni422@gmail.com>
2024-03-08 00:02:55 +0530

    added dockerfile

--- app/src/routes/instructer.ts
+++ app/src/routes/instructer.ts
@@ -1,3 +1,4 @@
+
 import express from "express";
 import { createcourse } from "../controller/createcourse";
 import { instructercontroller } from "../controller/createinstructer";
--- app/src/routes/learner.ts
+++ app/src/routes/learner.ts
@@ -1,11 +1,12 @@
+
 import express from "express";
 import { learnercontroller } from "../controller/createlearner";
 import { createlead } from "../controller/createleadcontroller";
 
 export const learner = express.Router();
 
+learner.post("/createlearner", learnercontroller);
+learner.post("/register", createlead);
 
 
-learner.post("/createlearner",learnercontroller)
 
-learner.post("/register",createlead) 
